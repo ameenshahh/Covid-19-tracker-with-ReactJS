ab025c7863cf7df90d52db905a23f664c9041ea7
Ameen Shah <ameenshahshajahan@gmail.com>
2021-01-02 22:02:24 +0530

    creates infocards and table displaying sorted list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import {
   FormControl,
   MenuItem,
@@ -7,6 +7,10 @@ import {
   CardContent
 } from '@material-ui/core';
 import './App.css';
+import InfoBox from './InfoBox';
+import Map from './Map';
+import Table from './Table';
+import { sortData } from "./Util"
 
 
 
@@ -17,6 +21,18 @@ import './App.css';
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide')
+  const [countryInfo, setCountryInfo] = useState({})
+  const [tableData, setTableData] = useState([])
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then(response => response.json())
+      .then((data) => {
+        setCountryInfo(data)
+      })
+  }, [])
+
+
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -29,41 +45,85 @@ function App() {
               value: country.countryInfo.iso2
             }
           ))
+
+          const sortedData = sortData(data)
+          setTableData(sortedData)
           setCountries(countries)
         })
     }
     getCountriesData();
   }, []);
 
-  const onCountryChange = (event) => {
+  const onCountryChange = async (event) => {
     const countryCode = event.target.value;
     setCountry(countryCode)
+
+    const url =
+      countryCode === "worldwide" ?
+        "https://disease.sh/v3/covid-19/all" :
+        `https://disease.sh/v3/covid-19/countries/${countryCode}`
+
+    await fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setCountry(countryCode)
+        setCountryInfo(data)
+      })
+
   }
+  // console.log(countryInfo)
+
 
 
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>Covid-19 Tracker App</h1>
-        <FormControl className="app__dropdown">
-          <Select
-            variant="outlined"
-            onChange={onCountryChange}
-            value={country}
-          >
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Option 2</MenuItem>
-            <MenuItem value="worldwide">Option 3</MenuItem>
-            <MenuItem value="worldwide">Option 4</MenuItem> */}
-          </Select>
-        </FormControl>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>Covid-19 Tracker App</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox
+            title="Coronavirus Cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+
+          <InfoBox
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+
+          <InfoBox
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
+        </div>
+
+        <Map />
       </div>
-
-
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live cases by country</h3>
+          <Table countries={tableData} />
+          < h3 > Worldwide new cases</h3>
+        </CardContent>
+      </Card>
     </div >
   );
 }
